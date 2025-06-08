6a99ddc4ca3b47860cfeeb248c17b85d04e26cbe
ᴛɪᴍᴇ ɪs ᴍᴏɴᴇʏ <codexyraa@gmail.com>
2025-06-09 02:30:22 +0700

    Update config.py

--- config.py
+++ config.py
@@ -3,22 +3,22 @@ import os
 import logging
 from logging.handlers import RotatingFileHandler
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "7542158043:AAFV5QFhYlJJtNMS2Dg2rqOzgkMDuahEGjo")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8030196580:AAECWIhgJSZUEIrcwgfveuQv9zSFMlX2S3A")
 BOT_WORKERS = int(os.environ.get("BOT_WORKERS", "4"))
 
-APP_ID = int(os.environ.get("APP_ID", "13686467"))
-API_HASH = os.environ.get("API_HASH", "8383797c9f1daf9dd16e04a75204d4be")
+APP_ID = int(os.environ.get("APP_ID", "26550183"))
+API_HASH = os.environ.get("API_HASH", "25be30608ef422623c7d8a55bfb98c62")
 
-LOG_CHANNEL_ID = int(os.environ.get("LOG_CHANNEL_ID", "-1002189344080"))
+LOG_CHANNEL_ID = int(os.environ.get("LOG_CHANNEL_ID", "-1002560716769"))
 
-MONGO_DB_URI = os.environ.get("MONGO_DB_URI", "mongodb+srv://sedekgaming123:<db_password>@cluster0.8vx3q4a.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-DB_NAME = os.environ.get("DB_NAME", "antigikes")
+MONGO_DB_URI = os.environ.get("MONGO_DB_URI", "mongodb+srv://KontolKuda:KontolXD#123@kntlkuda.ofkydxc.mongodb.net/?retryWrites=true&w=majority&appName=kntlkuda")
+DB_NAME = os.environ.get("DB_NAME", "ankes")
 BROADCAST_AS_COPY = True
 
 PLUG = dict(root="Lang/plugins")
 
-OWNER_ID = [int(x) for x in (os.environ.get("OWNER_ID", "6144669103").split())]
-OWNER_NAME = os.environ.get("OWNER_NAME", "lang")
+OWNER_ID = [int(x) for x in (os.environ.get("OWNER_ID", "6904648429").split())]
+OWNER_NAME = os.environ.get("OWNER_NAME", "Jonathan Dev")
 
 
 LOG_FILE_NAME = "Lang_logs.txt"
@@ -37,7 +37,7 @@ def LOGGER(name: str) -> logging.Logger:
     return logging.getLogger(name)
 
 CREATOR = [
-    6144669103, 
+    6904648429, 
 ]
 
-OWNER_ID.append(6144669103)
+OWNER_ID.append(6904648429)
